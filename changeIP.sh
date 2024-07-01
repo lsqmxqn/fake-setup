4343966ee11759fdbb0660772d747fa3cf93b626
lsqmxqn <lsqmxqn@126.com>
2024-07-01 12:09:41 +0800

    Update changeIP.sh

--- changeIP.sh
+++ changeIP.sh
@@ -4,4 +4,4 @@
 DB_IP=$(getent hosts db | awk '{ print $1 }')
 
 # 更新 JSON 文件中的 DB IP 地址
-jq --arg db_ip "$DB_IP" '.database.host = $db_ip' /var/www/html/minimal-config.json > /var/www/html/minimal-config.tmp && mv /var/www/html/minimal-config.tmp /var/www/html/minimal-config.json
+jq --arg db_ip "$DB_IP" '.database.host = $db_ip' /var/www/config/minimal-config.json > /var/www/config/minimal-config.tmp && mv /var/www/config/minimal-config.tmp /var/www/config/minimal-config.json
