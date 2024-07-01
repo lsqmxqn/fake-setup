0746b273b13ceb522438d210c3b8c0bb8325fb22
lsqmxqn <lsqmxqn@126.com>
2024-07-01 11:33:32 +0800

    Create changeIP.sh

---
+++ changeIP.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# 获取 db 容器的 IP 地址
+DB_IP=$(getent hosts db | awk '{ print $1 }')
+
+# 更新 JSON 文件中的 DB IP 地址
+jq --arg db_ip "$DB_IP" '.database.host = $db_ip' /var/www/html/minimal-config.json > /var/www/html/minimal-config.tmp && mv /var/www/html/minimal-config.tmp /var/www/html/minimal-config.json
+
+# 启动 Apache 或者 PHP 服务器
+apache2-foreground
